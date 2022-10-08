683f8a01d6b462a10445a9d8263fe641bd2aff6d
rugerclaus <rogerrock12games@gmail.com>
2022-10-07 19:20:21 -0600

    added component feature

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ import { Mobile } from "./js/Mobile.js"
 const screenWidth = window.innerWidth
 const screenHeight = window.innerHeight
 
-const home = fetch('index.html')
+const home = fetch('components/home.html')
+const projects = fetch('components/projects.html')
 
 const links = document.querySelector('nav')
 const nav = document.querySelector('.navigation')
 const footer = document.querySelector('.footer')
+const main = document.querySelector('main')
 
 const today = new Date()
 
 footer.innerHTML = `Copyright Claussy Designs ${today.getFullYear()}`
 
-nav.addEventListener('click', () => {
+nav.addEventListener('mouseover', () => {
     Mobile.displayNav(links, nav)
 })
 
@@ -26,5 +28,22 @@ links.addEventListener('mouseover', (e) => {
 links.addEventListener('mouseout', (e) => {
     if(e.target.classList.contains('selectedLink')) {
         UI.deselect(e.target)
+    }
+})
+
+links.children.item(0).addEventListener('click', (e) => {
+    if(e.target.id === 'home'){
+        home.then(res => {
+            res.text().then(content => {
+                main.innerHTML = content
+            })
+        })
+    }
+    if(e.target.id === 'projects'){
+        projects.then(res => {
+            res.text().then(content => {
+                main.innerHTML = content
+            }) 
+        })
     }
 })
