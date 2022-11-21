68c34da6fa19f4bdf2058699496d74c893e044bd
rugerclaus <rogerrock12games@gmail.com>
2022-11-21 08:47:30 -0700

    Reset

--- README.md
+++
@@ -1,3 +0,0 @@
-# Claussy-Designs
-
-# includes a router that I made with the fetch API which fetches all the pages on the load of the page and appends them upon navigation
--- mail.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $to = 'rogerrock12games@gmail.com';
-    $headers = 'From: mail@claussydesigns.com';
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    $name = $_POST['name'];
-    mail($to,$subject,$message,$headers);
-?>
