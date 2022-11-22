53600239361b92d1fde962a5ff5989db5ddb623b
rugerclaus <rogerrock12games@gmail.com>
2022-11-22 15:37:52 -0700

    updated home page skills section

--- frontend/views/home.php
+++ frontend/views/home.php
@@ -14,19 +14,12 @@
         
         <div class="skillset">
             <h1>Skills:</h1>
-            <div class="labels">
-                <h2>HTML5:</h2>
-                <h2>CSS3:</h2>
-                <h2>JavaScript:</h2>
-                <h2>PHP:</h2>
-                <h2>MySQL:</h2>
-            </div>
             <div class="skillList">
-                <p>I have many years of experience with HTML5. Starting when I was 12 years old and moving on since then, I have become very skilled at semantic HTML and using HTML to style my pages along with CSS.</p>
-                <p>CSS is my friend and nemesis, however I am skilled enough with it to create eye popping layouts and animations.</p>
-                <p>My experience with JavaScript is mostly frontend and vanilla, however I do know how to use parts of the ReactJS library, which makes designing UIs a thousand times easier!</p>
-                <p>PHP is everywhere. In fact it's what's allowing the site to all be rendered on the same page! I have quite a solid understanding of PHP and I really enjoy implementing object oriented patterns.</p>
-                <p>MySQL is very handy if you want to build a store, or handle large amounts of data. I have a strong understanding of SQL and use it extensively in my projects.</p>
+                <label for="html"><h2>HTML5:</h2></label><p name='html'>I have many years of experience with HTML5. Starting when I was 12 years old and moving on since then, I have become very skilled at semantic HTML and using HTML to style my pages along with CSS.</p>
+                <label for="css"><h2>CSS3:</h2></label><p name='css'>CSS is my friend and nemesis, however I am skilled enough with it to create eye popping layouts and animations.</p>
+                <label for="js"><h2>JavaScript:</h1></label></label><p name='js'>My experience with JavaScript is mostly frontend and vanilla, however I do know how to use parts of the ReactJS library, which makes designing UIs a thousand times easier!</p>
+                <label for="php"><h2>PHP:</h2></label><p name='php'>PHP is everywhere. In fact it's what's allowing the site to all be rendered on the same page! I have quite a solid understanding of PHP and I really enjoy implementing object oriented patterns.</p>
+                <label for="mysql"><h2>MySQL:</h2></label><p name='mysql'>MySQL is very handy if you want to build a store, or handle large amounts of data. I have a strong understanding of SQL and use it extensively in my projects.</p>
             </div>
         </div>
     </div>
